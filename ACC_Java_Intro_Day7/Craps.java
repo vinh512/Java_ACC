c2f15cbe91c01d42fd5d605aa4146b939bba02be
vinh huynh <vvvhuynh@yahoo.com>
2017-09-27 15:24:18 -0500

    changed switch to if statements

--- ACC_Java_Intro_Day7/Craps.java
+++ ACC_Java_Intro_Day7/Craps.java
@@ -3,7 +3,7 @@
 	whether you won, lost, or need to continue rerolling for the point value.
 
 	@Author Vinh Huynh
-	@version 1.1 - reworked using the concept of states
+	@version 1.2 - reworked using if-else statements 
 */
 
 public class Craps {
@@ -14,27 +14,23 @@ public class Craps {
 		int state, point = 0;
 		int sum = rollDice();
 		
-		switch (sum) {			
-			case 2: case 3: case 12: // Win conditions 
-				state = LOSE; 
-				break;			
-			case 7: case 11: // Lose conditions
-				state = WIN; 
-				break;			
-			default: // Reroll conditions
-				state = POINT;
-				point = sum; // Stores the sum value to the point variable									
-				
-				while (state == POINT) {
-					System.out.println("Your point is " + point + ". Roll again!");
-					sum = rollDice();
-					if (sum == 7) 
-						state = LOSE;				
-					else if (sum == point) 
-						state = WIN;									 										
-				}				
-				break;
-		} // end of switch
+		if (sum == 2 || sum == 3 || sum == 12)			 
+			state = LOSE; 
+		else if (sum == 7 || sum == 11)				
+			state = WIN; 
+		else {
+			state = POINT;
+			point = sum; // Stores the sum value to the point variable									
+		}		
+		
+		while (state == POINT) {
+			System.out.println("Your point is " + point + ".");
+			sum = rollDice();
+			if (sum == 7) 
+				state = LOSE;				
+			else if (sum == point) 
+				state = WIN;									 										
+		}								
 		
 		if (state == WIN) 
 			System.out.println("You Win!");
@@ -44,12 +40,11 @@ public class Craps {
 	
 	// Gets the sum of dice rolls and displays a result message
 	private static int rollDice() {
-		// Prompts user to press enter key
 		System.console().readLine("Press enter to roll the dice!: ");		
 		int die1 = (int)(Math.random() * 6) + 1;
 		int die2 = (int)(Math.random() * 6) + 1;
 		int sum = die1 + die2;
-		System.out.println("You rolled " + die1 + " + " + die2 + " = " + sum + ".");
+		System.out.println("\nYou rolled " + die1 + " + " + die2 + " = " + sum + ".");
 		return sum;
 	}
 }
