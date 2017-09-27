bc2290a96dca3db632bfcfb71634430227b82746
vinh huynh <vvvhuynh@yahoo.com>
2017-09-27 15:07:01 -0500

    reworked using states

--- ACC_Java_Intro_Day7/Craps.java
+++ ACC_Java_Intro_Day7/Craps.java
@@ -3,67 +3,53 @@
 	whether you won, lost, or need to continue rerolling for the point value.
 
 	@Author Vinh Huynh
-	@version 1.0
+	@version 1.1 - reworked using the concept of states
 */
 
 public class Craps {
 	
 	public static void main(String[] args) {					
 		
-		int dice1, dice2, sum, point;
+		final int LOSE = 0, WIN = 1, POINT = 2;
+		int state, point = 0;
+		int sum = rollDice();
 		
-		dice1 = getRandomNum();
-		dice2 = getRandomNum();		
-		sum = dice1 + dice2; 
-		
-		switch (sum) {
-			// Win conditions
-			case 2: case 3: case 12: 
-				System.out.println(printMsg(dice1, dice2, sum, "lose")); break;
-			// Lose conditions
-			case 7: case 11:
-				System.out.println(printMsg(dice1, dice2, sum, "win")); break;
-			// Reroll conditions
-			default:
-				point = sum; // Stores the sum value to the point variable					
-				System.out.println(printMsg(dice1, dice2, sum, ""));
-				System.out.println("Your point number is " + point + ". Roll it again to win!");								 
+		switch (sum) {			
+			case 2: case 3: case 12: // Win conditions 
+				state = LOSE; 
+				break;			
+			case 7: case 11: // Lose conditions
+				state = WIN; 
+				break;			
+			default: // Reroll conditions
+				state = POINT;
+				point = sum; // Stores the sum value to the point variable									
 				
-				// Continue rerolling for point value
-				do { 
-					dice1 = getRandomNum();
-					dice2 = getRandomNum();
-					sum = dice1 + dice2;
-					
-					if (sum == 7) {
-						System.out.println(printMsg(dice1, dice2, sum, "lose"));
-						return;
-					} else if (sum == point) {
-						System.out.println(printMsg(dice1, dice2, sum, "win"));
-						return;
-					} 					
-					System.out.println(printMsg(dice1, dice2, sum, "roll"));					
-				} while (sum != 7 || sum != point);
-				
+				while (state == POINT) {
+					System.out.println("Your point is " + point + ". Roll again!");
+					sum = rollDice();
+					if (sum == 7) 
+						state = LOSE;				
+					else if (sum == point) 
+						state = WIN;									 										
+				}				
 				break;
-		} // end of switch		
-	}
-	
-	// Generates a random integer from 1-to-6
-	private static int getRandomNum() {
-		return (int)(Math.random() * 6) + 1;
-	}
-	
-	// Creates a String of the results
-	private static String printMsg(int d1, int d2, int sum, String result) {
-		String appendMsg = "";
+		} // end of switch
 		
-		switch (result) {
-			case "lose": appendMsg = " You lose!"; break; 
-			case "win" : appendMsg = " You win!"; break;
-			case "roll": appendMsg = " Roll again!"; break;
-			default:     appendMsg = ""; break;
-		}				
-		return "You rolled " + d1 + " and " + d2 + " for a total of " + sum + "." + appendMsg;
+		if (state == WIN) 
+			System.out.println("You Win!");
+		else if (state == LOSE)
+			System.out.println("You Lose!"); 			
+	}
+	
+	// Gets the sum of dice rolls and displays a result message
+	private static int rollDice() {
+		// Prompts user to press enter key
+		System.console().readLine("Press enter to roll the dice!: ");		
+		int die1 = (int)(Math.random() * 6) + 1;
+		int die2 = (int)(Math.random() * 6) + 1;
+		int sum = die1 + die2;
+		System.out.println("You rolled " + die1 + " + " + die2 + " = " + sum + ".");
+		return sum;
 	}
 }
