7aae8957af7ce9ea78968f2ba7e6fab97ab8742e
vinh huynh <vvvhuynh@yahoo.com>
2017-10-01 18:39:41 -0500

    create rollDice method

--- ACC_Java_Intro_Day7/Craps_OO/CrapsGame.java
+++ ACC_Java_Intro_Day7/Craps_OO/CrapsGame.java
@@ -1,19 +1,28 @@
 public class CrapsGame {
+	
 	public static void main(String[] args) {
-	
-	Die die1 = new Die();
-	Die die2 = new Die();
-	
-	int d1 = die1.getValue();
-	int d2 = die2.getValue();
-	
-	System.out.printf("d1: %d\n", d1);
-	System.out.printf("d2: %d\n", d2);
-
-	// Since I used the static modifier for the getSum method,
-	// it requires the class (Die) to invoke
-	int sum = Die.getSum(d1, d2);
-
-	System.out.printf("sum: %d\n", sum);
+		
+		int LOSE = 0, WIN = 1, POINT = 2; 
+		int state, point;
+		
+		rollDice();	
 	}
+	
+	
+	private static void rollDice() {
+		Die die1 = new Die();
+		Die die2 = new Die();
+		
+		int d1 = die1.getValue();
+		int d2 = die2.getValue();
+		
+		System.out.printf("d1: %d\n", d1);
+		System.out.printf("d2: %d\n", d2);
+		
+		// Since I used the static modifier for the getSum method,
+		// it requires the class (Die) to invoke
+		int sum = Die.getSum(d1, d2);
+		System.out.printf("sum: %d\n", sum);		
+	}
+	
 }
