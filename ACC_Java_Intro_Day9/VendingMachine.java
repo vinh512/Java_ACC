78f2233ae015bcb47622a981f663889ca084c11a
vinh huynh <vvvhuynh@yahoo.com>
2017-10-04 18:18:32 -0500

    made beer cost a constant

--- ACC_Java_Intro_Day9/VendingMachine.java
+++ ACC_Java_Intro_Day9/VendingMachine.java
@@ -1,6 +1,6 @@
 /*
 	Write a java program to simulates a vending machine that accepts four coin
-	denominations and returns the coins if no item is available
+	denominations and returns the change and/or coins if no item is available
 */
 
 import java.util.Scanner; 
@@ -10,15 +10,16 @@ public class VendingMachine {
 	public static void main(String[] args) {
 		
 		Scanner input = new Scanner(System.in);
+		final double BEER_COST = 1.75;
 		int beerCount = 5;
 		double total = 0;
 		String coin;
 		
 		do {
 			System.out.println("\nH=$.50, Q=$.25, D=$.10, N=$.05");
-			System.out.println("Please insert $1.75 for your beer");
+			System.out.println("Please insert $1.75 for your beer.");
 			
-			while (total < 1.75) {	
+			while (total < BEER_COST) {	
 				System.out.print("Enter coins: ");
 				coin = input.nextLine().toUpperCase();
 				
@@ -31,16 +32,15 @@ public class VendingMachine {
 				}		
 			}
 					
-			if (total >= 1.75) {
+			if (total >= BEER_COST) {
 				System.out.println("Dispensing you beer - Enjoy!");
 				beerCount--;
-				if (total > 1.75) 
+				if (total > BEER_COST) 
 					System.out.printf("You inserted $%.2f. Here is your change: $%.2f\n", 
-									   total, total - 1.75);				
+									   total, total - BEER_COST);				
 			}
 			
-			total = 0; // Resets total to repeat while loop
-			
+			total = 0; // Resets total to repeat while loop			
 		} while (beerCount != 0); // End of do-while loop 
 		
 		System.out.println("Out of Beers!");
