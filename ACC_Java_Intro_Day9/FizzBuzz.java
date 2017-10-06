c3b00e0263d66b60fac7926f80777623a69e38eb
vinh huynh <vvvhuynh@yahoo.com>
2017-10-05 21:49:42 -0500

    added another solution

--- ACC_Java_Intro_Day9/FizzBuzz.java
+++ ACC_Java_Intro_Day9/FizzBuzz.java
@@ -6,9 +6,21 @@ public class FizzBuzz {
 	
 	public static void main(String[] args) {
 		
+		// Returns the current value of the running JVM time source in nanoseconds
 		long start = System.nanoTime();
 		
 		for (int n = 1; n <= 100; n++) {
+			
+			if (n % 3 == 0 && n % 5 == 0) 
+				System.out.println("FizzBuzz");
+			else if (n % 3 == 0)
+				System.out.println("Fizz");
+			else if (n % 5 == 0) 
+				System.out.println("Buzz");
+			else
+				System.out.println(n);
+			
+			/*
 			String output = "";
 			if (n % 3 == 0)
 				output += "Fizz";
@@ -17,10 +29,11 @@ public class FizzBuzz {
 			if (output.length() == 0)
 				output += n;
 			System.out.println(output);
+			*/
 		}
 		
 		long end = System.nanoTime();
+		// Divide by a billion to convert ns to seconds
 		System.out.printf("Execution in %.3f seconds\n", (end-start) / 1.0e9);		
 	}
-
 }
