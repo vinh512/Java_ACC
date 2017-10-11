4226d301910e1ee71a35f787581438da01763c80
vinh huynh <vvvhuynh@yahoo.com>
2017-10-10 20:35:12 -0500

    modularized code

--- ACC_Java_I_Day1/Hello2017.java
+++ ACC_Java_I_Day1/Hello2017.java
@@ -1,5 +1,5 @@
 // we need to create a directory of folders (edu, acc, java1, and day1 with the .class file
-package edu.acc.java1.day1;
+//package edu.acc.java1.day1;
 
 // by importing this, we don't need to type out 'System'
 import static java.lang.System.out;
@@ -8,27 +8,38 @@ import javax.swing.JOptionPane;
 public class Hello2017 {
 
   public static void main(String[] args) {
-      // provide a predefined array of greeting messages
-      final String[] GREETINGS = {"Hello", "Wassup", "Yo", "Hi", "Howdy"};
-
-      // generates of random number from 0 - 4
-      final String GREETING = GREETINGS[ (int)(Math.random() * GREETINGS.length) ];
-
-      // declare name variable
-      String name;
-
-      // do until user enters valid name
-      do  {
-        name = System.console().readLine("What's your name? ");
-      } while (name.isEmpty());
-
-      // builds a string and assigns it to RESPONSE constant
-      final String RESPONSE = String.format("%s, %s\n", GREETING, name);
-
-      // prints out the message
-      out.print(RESPONSE);
-
-      // opens a dialog prompt displaying the greeting message
-      JOptionPane.showMessageDialog(null, RESPONSE);
+      // get a random greeting
+      final String GREETING = getRandomGreeting();
+      // build a RESPONSE
+      final String RESPONSE = buildResponse(GREETING);
+      // display the response
+      display(RESPONSE);
   }
+
+  private static String getRandomGreeting() {
+    // provide a predefined array of greeting messages
+    final String[] GREETINGS = {"Hello", "Wassup", "Yo", "Hi", "Howdy"};
+    // generates of random number from 0 - 4
+    final String GREETING = GREETINGS[ (int)(Math.random() * GREETINGS.length) ];
+    return GREETING;
+  }
+
+  private static String buildResponse(final String GREETING) {
+    String name;
+    // do until user enters valid name
+    do  {
+      name = System.console().readLine("What's your name? ");
+    } while (name.isEmpty());
+    // builds a string and assigns it to RESPONSE constant
+    final String RESPONSE = String.format("%s, %s\n", GREETING, name);
+    return RESPONSE;
+  }
+
+  private static void display(final String RESPONSE) {
+    // prints out the message
+    out.print(RESPONSE);
+    // opens a dialog prompt displaying the greeting message
+    JOptionPane.showMessageDialog(null, RESPONSE);
+  }
+
 }
