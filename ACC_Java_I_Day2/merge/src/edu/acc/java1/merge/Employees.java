0ee4b988207110a32136bca0a3cb23e0f707c613
Vinh512 <vvvhuynh@yahoo.com>
2017-10-14 15:55:37 -0500

    add logic statements

---
+++ ACC_Java_I_Day2/merge/src/edu/acc/java1/merge/Employees.java
@@ -0,0 +1,7 @@
+/*
+	This is the Employees class used to create instances of Employees objects
+*/
+
+public class Employees {
+	// An employee has a name and an email address as fields
+}
--- ACC_Java_I_Day2/merge/src/edu/acc/java1/merge/Merge.java
+++ ACC_Java_I_Day2/merge/src/edu/acc/java1/merge/Merge.java
@@ -1,5 +1,19 @@
+/*
+	Let's simulate mail merge. There are three recipients: Alice, Bob, and Gene.
+	Their email addresses are, respectively, aa@widgets.inc, bb@widgets.inc, and
+	gg@geocities.com. The message is addressed from supplier@thingamajig.com to 
+	each of the three and says "Hello <name>, Thingamajig.com would like to be 
+	your new very best number one supplier. Contact us for more details."
+	
+	Write a Java program to simulate sending the message to each of the three 
+	employees at Widgets, Inc. with their name substituted in the appropriate 
+	place in the message. Just display the completed message to indicate it was 
+	"sent".
+*/
+
 public class Merge {
 	public static void main(String[] args) {
-		
+		// I have 3 employees
+		// I print a message to each one
 	}
 }
