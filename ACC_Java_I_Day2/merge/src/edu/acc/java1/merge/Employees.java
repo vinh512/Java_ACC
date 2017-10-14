7e04a16eea6f99e9e540a3f90bf0921b2dd31c15
Vinh512 <vvvhuynh@yahoo.com>
2017-10-14 16:01:05 -0500

    add getters and setters to Employees class

--- ACC_Java_I_Day2/merge/src/edu/acc/java1/merge/Employees.java
+++ ACC_Java_I_Day2/merge/src/edu/acc/java1/merge/Employees.java
@@ -4,4 +4,22 @@
 
 public class Employees {
 	// An employee has a name and an email address as fields
+	private String name;
+	private String email;
+	
+	public String getName() {
+		return name;	
+	}
+	
+	public String getEmail() {
+		return email;	
+	}
+	
+	public void setName(String name) {
+		this.name = name;	
+	}
+	
+	public void setEmail(String email) {
+		this.email = email;	
+	}
 }
