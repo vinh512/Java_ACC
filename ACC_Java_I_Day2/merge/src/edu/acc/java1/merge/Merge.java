d6d95f32c32e698e71d41579994aaef87e230a9d
vinh huynh <vvvhuynh@yahoo.com>
2017-10-15 14:49:32 -0500

    fix variable plurals

--- ACC_Java_I_Day2/merge/src/edu/acc/java1/merge/Merge.java
+++ ACC_Java_I_Day2/merge/src/edu/acc/java1/merge/Merge.java
@@ -14,20 +14,20 @@
 public class Merge {
 	public static void main(String[] args) {
 		// create an array to hold 3 Employee objects
-		Employees[] employees = new Employees[3];
+		Employees[] employee = new Employees[3];
 		
 		// populate array with instances of Employees
-		employees[0] = new Employees("Alice", "aa@widgets.inc");
-		employees[1] = new Employees("Bob", "bb@widgets.inc");
-		employees[2] = new Employees("Gene", "gg@geocities.com");
+		employee[0] = new Employees("Alice", "aa@widgets.inc");
+		employee[1] = new Employees("Bob", "bb@widgets.inc");
+		employee[2] = new Employees("Gene", "gg@geocities.com");
 		
 		// iterate through array and call print method
-		for (int i = 0; i < employees.length; i++) 
-			printEmail(employees[i]);			
+		for (int i = 0; i < employee.length; i++) 
+			printMsg(employee[i]);			
 	}
 	
 	// prints out the email message with the corresponding employee
-	private static void printEmail(Employees recipient) {
+	private static void printMsg(Employees recipient) {
 		final String NAME = recipient.getName();
 		System.out.printf("\nFrom: Huckster <supplier@thingamajig.com>\n");
 		System.out.printf("To: %s <%s>\n", NAME, recipient.getEmail());
