3862077754e35230ea0e9c08211545a8ee12a870
vinh huynh <vvvhuynh@yahoo.com>
2017-10-20 19:02:00 -0500

    clarified naming

--- ACC_Java_I_Day2/merge/src/edu/acc/java1/merge/MailMerge.java
+++ ACC_Java_I_Day2/merge/src/edu/acc/java1/merge/MailMerge.java
@@ -2,22 +2,25 @@ public class MailMerge {
 
 	public final Employees SENDER;
 
+	// specifies the sender whom is Employees object (Huckster
 	public MailMerge(Employees sender) {
 		SENDER = sender;
 	}
-	
+
+	// prints the message for each of the recipients
 	public void send(Employees[] recipients) {
 		final String S_NAME = SENDER.getName();
-		final String S_EMAIL = SENDER.getEmail();		
-		
-		String msg = "";		
-		
-		for (Employees x : recipients) {
-			msg = String.format("\nFrom: %s <%s>\n" +
+		final String S_EMAIL = SENDER.getEmail();	
+
+		for (Employees person : recipients) {			
+			String name = person.getName();
+			String email = person.getEmail();
+			
+			String msg = String.format("\nFrom: %s <%s>\n" +
 			                "To: %s <%s>\nHello %s, Thingamajig.com would like " +
 			                "to be your new very best number one supplier. " + 
 			                "Contact us for more details.", S_NAME, S_EMAIL, 
-			                x.getName(), x.getEmail(), x.getName());
+			                name, email, name);
 			
 			System.out.println(msg);		
 		}
