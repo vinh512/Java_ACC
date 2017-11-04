1f28891992d1fe71e6ce58aefaaebb3f24c0dff4
vinh huynh <vvvhuynh@yahoo.com>
2017-11-04 18:23:45 -0500

    clean up code

--- ACC_Java_II_Day2/word_counter/src/edu/acc/java2/word_counter/WordCounter.java
+++ ACC_Java_II_Day2/word_counter/src/edu/acc/java2/word_counter/WordCounter.java
@@ -1,49 +1,41 @@
 package edu.acc.java2.word_counter;
 
 import java.io.*;
-import java.util.Arrays;
 
 public class WordCounter {
+
   public static void main(String[] args) {
-      if (args.length != 2) {
-        System.out.println("Usage: java edu.acc.java.word_counter.WordCounter [txt filename] [word-to-count] ");
-        return;
+    if (args.length != 2) {
+      System.out.println("Usage: java edu.acc.java.word_counter.WordCounter [txt filename] [word-to-count] ");
+      return;
+    }
+
+    try (BufferedReader br = new BufferedReader(new FileReader(args[0]))) {
+      String line;
+      int countTotal = 0;
+
+      while ((line = br.readLine()) != null) {
+        countTotal += countMatch(line, args[1]);
       }
-
-      try (BufferedReader br = new BufferedReader(new FileReader(args[0])))
-      {
-        String line;
-        int countTotal = 0;
-
-        while ((line = br.readLine()) != null) {
-          System.out.println(args[1]);
-          //find word and increment count
-          countTotal += countMatch(line, args[1]);
-        }
-
-        System.out.println(countTotal);
-
-      } catch (IOException e) {
-        e.printStackTrace();
-        System.out.println("Can't find file");
-
-      } catch (Exception e) {
-        System.out.println("Error?");
-      }
+      System.out.printf("The word \"%s\" appeared %d times.\n", args[1], countTotal);
+    }
+    catch (IOException e) {
+      System.out.println("Possibly can't find file");
+    }
+     catch (Exception e) {
+      System.out.println("Generic Error");
+    }
   }
 
   private static int countMatch(String line, String wordSearch) {
     int count = 0;
-    //String[] wordArray = line.split(" ");
     String[] wordArray = line.replaceAll("[^a-zA-z]", " ").split(" ");
-    System.out.println(Arrays.toString(wordArray));
 
     for (int i = 0; i < wordArray.length; i++) {
       if (wordSearch.equals(wordArray[i].toLowerCase())) {
         count++;
       }
     }
-    System.out.println(count);
     return count;
   }
 
