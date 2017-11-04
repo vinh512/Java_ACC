9d396c2305b09fc08e9df3cdd41b9a35b1898e47
vinh huynh <vvvhuynh@yahoo.com>
2017-11-04 16:40:59 -0500

    split lines into arrays

--- ACC_Java_II_Day2/word_counter/src/edu/acc/java2/word_counter/WordCounter.java
+++ ACC_Java_II_Day2/word_counter/src/edu/acc/java2/word_counter/WordCounter.java
@@ -1,23 +1,43 @@
 package edu.acc.java2.word_counter;
 
+import java.io.*;
+import java.util.Arrays;
+
 public class WordCounter {
 
   public static void main(String[] args) {
       if (args.length != 2) {
-        System.out.println("Usage: java edu.acc.java.WordCounter [txt filename] [word] ");
+        System.out.println("Usage: java edu.acc.java.word_counter.WordCounter [txt filename] [word] ");
         return;
       }
 
-      try (BufferedReader br = new BufferedReader(new FileReader(args[0]))) {
-        String line;
-        while ((line = br.readLine()) != null) {
+      System.out.println("Hello");
 
-          System.out.println("test");
+      try (BufferedReader br = new BufferedReader(new FileReader(args[0])))
+      {
+        String line;
+        String word = args[1];
+        String wordArray[];
+
+        int count = 0;
+
+
+        while ((line = br.readLine()) != null) {
+          System.out.println(args[1]);
+          //find word and increment count
+          //countMatch();
+          wordArray = line.split(" ");
+          System.out.println(Arrays.toString(wordArray));
+
 
         }
 
       } catch (IOException e) {
+        e.printStackTrace();
+        System.out.println("Can't find file");
 
+      } catch (Exception e) {
+        System.out.println("Error?");
       }
 
   }
