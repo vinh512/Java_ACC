5b1adc0d944b5b26ff2b031b640c4c86ea1884f7
vinh huynh <vvvhuynh@yahoo.com>
2017-11-04 20:47:40 -0500

    add comments

--- ACC_Java_II_Day2/word_counter/src/edu/acc/java2/word_counter/WordCounter.java
+++ ACC_Java_II_Day2/word_counter/src/edu/acc/java2/word_counter/WordCounter.java
@@ -1,15 +1,22 @@
 package edu.acc.java2.word_counter;
 
+/*
+  The program searches through a user-specified text file and counts the number
+  of times a user-specified word occurs in the file.
+  Usage: java edu.acc.java.word_counter.WordCounter [txt filename] [word]
+*/
+
 import java.io.*;
 
 public class WordCounter {
 
   public static void main(String[] args) {
+    // ensures 2 arguments are provided
     if (args.length != 2) {
       System.out.println("Usage: java edu.acc.java.word_counter.WordCounter [txt filename] [word]");
       return;
     }
-
+    // gives meaning behind the user-provided arguments
     final String FILE = args[0];
     final String WORD_MATCH = args[1];
 
@@ -17,6 +24,7 @@ public class WordCounter {
       String line;
       int countTotal = 0;
 
+      // keeps reading lines until end-of-file and increments count total
       while ((line = br.readLine()) != null) {
         countTotal += countMatch(line, WORD_MATCH);
       }
@@ -31,8 +39,10 @@ public class WordCounter {
     }
   }
 
+  // searches for the user-specified word in a String-turned-Array & keeps count
   private static int countMatch(String line, String wordSearch) {
     int count = 0;
+    // removes punctuation and converts String into an array
     String[] wordArray = line.replaceAll("[^a-zA-z]", " ").split(" ");
 
     for (String word : wordArray) {
