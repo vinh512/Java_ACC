91be18c13c90b60ca760be4805b31c69fb1721ea
Vinh512 <vvvhuynh@yahoo.com>
2017-11-10 23:26:47 -0600

    add regex pattern

--- ACC_Java_II_Day4/Main.java
+++ ACC_Java_II_Day4/Main.java
@@ -1,9 +1,22 @@
 package edu.acc.java2.taxes;
 
+/*
+    Use the enormous tax data set from 2014 linked below to deploy an executable 
+    jar named taxes.jar whose main program displays the total number of income tax 
+    returns filed in the data set, the total amount of taxed income reported, and 
+    the total amount of tax collected.
+*/
+
 public class Main {
 
     public static void main(String[] args) {
-      
+        if (args.length != 1) {
+            System.out.println("Usage: java -jar edu.acc.java2.taxes.jar [file csv]");
+            return;
+        }
+        
+        final String pattern = "([\\w]+\\.\\d+)(?:[^,]*,){119}([\\w]+\\.\\d+),([\\w]+\\.\\d+)";
+               
        
     }
     
