f658feed75a1dc3b7c6223884dd8fc61bbfcf747
Vinh512 <vvvhuynh@yahoo.com>
2017-11-11 06:16:23 -0600

    refactor code

--- ACC_Java_II_Day4/Main.java
+++ ACC_Java_II_Day4/Main.java
@@ -4,30 +4,34 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.text.DecimalFormat;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 /*
-    Use the enormous tax data set from 2014 linked below to deploy an executable 
-    jar named taxes.jar whose main program displays the total number of income tax 
-    returns filed in the data set, the total amount of taxed income reported, and 
-    the total amount of tax collected.
+    Use the enormous tax data set from 2014 to deploy an executable jar named
+    taxes.jar whose main program displays the total number of income tax returns
+    filed in the data set, the total amount of taxed income reported and the 
+    total amount of tax collected.
+    N1     - Number of returns
+    N11901 - Number of returns with tax due at time of filing
+    A11901 - Tax due at time of filing
 */
 
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("program started");
         if (args.length != 1) {
             System.out.println("Usage: java -jar edu.acc.java2.taxes.jar [file csv]");
             return;
         }
         
-        final String pattern = "([\\w]+\\.\\d+)(?:[^,]*,){119}([\\w]+\\.\\d+),([\\w]+\\.\\d+)";
-        final Pattern patt = Pattern.compile(pattern);
-        String strN1, strN11901, strA11901;
+        final String PATTERN = "([\\w]+\\.\\d+)(?:[^,]*,){119}([\\w]+\\.\\d+),"
+                               + "([\\w]+\\.\\d+)";
+        final Pattern PATT = Pattern.compile(PATTERN);
+        String N1, N11901, A11901;
         
         Map<String, Double> data = new HashMap<>();
         data.put("N1", 0.0);
@@ -36,25 +40,26 @@ public class Main {
         
         try (BufferedReader br = new BufferedReader(new FileReader(args[0]))) {
             String line;
-            System.out.println("entered try block");
-            
             while ((line = br.readLine()) != null) {
-                Matcher m = patt.matcher(line);
+                Matcher m = PATT.matcher(line);
                 
                 if (m.find()) {
-                    strN1 = m.group(1);
-                    strN11901 = m.group(2);
-                    strA11901 = m.group(3);
-                    System.out.println("N1: " + strN1);
-                    System.out.println("N19011: " + strN11901);
-                    System.out.println("A19011: " + strA11901);
+                    N1     = m.group(1);
+                    N11901 = m.group(2);
+                    A11901 = m.group(3);
                     
-                    update(data, strN1, strN11901, strA11901);   
+                    calcTotal(data, N1, N11901, A11901);   
                 }
             }
-            System.out.printf("The total of N1 is: %.4f\n", data.get("N1"));
-            System.out.printf("The total of N11901 is: %.4f\n", data.get("N11901"));
-            System.out.printf("The total of A11901 is: %.4f\n", data.get("A11901"));
+            
+            DecimalFormat df = new DecimalFormat("###,###,###,###,###.00");
+            
+            System.out.printf("The total of N1 is: $%s\n",
+                              df.format(data.get("N1")));
+            System.out.printf("The total of N11901 is: $%s\n",
+                              df.format(data.get("N11901")));
+            System.out.printf("The total of A11901 is: $%s\n",
+                              df.format(data.get("A11901")));
             
         } catch (FileNotFoundException fnfe){
             System.out.println("File not found");
@@ -63,18 +68,11 @@ public class Main {
         }
     }
     
-    private static void update(Map<String, Double> data, String strN1, String strN11901, String strA11901) {
-        double numN1     = Double.parseDouble(strN1); 
-        double numN11901 = Double.parseDouble(strN11901);
-        double numA11901 = Double.parseDouble(strA11901);        
-
-        Double newN1     = data.get("N1") + numN1;
-        Double newN11901 = data.get("N11901") + numN11901;
-        Double newA11901 = data.get("A11901") + numA11901;
-        
-        data.put("N1", newN1);
-        data.put("N11901", newN11901);
-        data.put("A11901", newA11901);
+    private static void calcTotal(Map<String, Double> data, String N1,
+                               String N11901, String A11901) {          
+        data.put("N1", data.get("N1") + Double.parseDouble(N1));
+        data.put("N11901", data.get("N11901") + Double.parseDouble(N11901));
+        data.put("A11901", data.get("A11901") + Double.parseDouble(A11901));
     }
     
 }
