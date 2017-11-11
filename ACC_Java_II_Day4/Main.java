b02f3d362f3753e81e6ade9815d876cc3cb4322c
Vinh512 <vvvhuynh@yahoo.com>
2017-11-10 23:49:13 -0600

    add buffered/file reader

--- ACC_Java_II_Day4/Main.java
+++ ACC_Java_II_Day4/Main.java
@@ -1,5 +1,12 @@
 package edu.acc.java2.taxes;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 /*
     Use the enormous tax data set from 2014 linked below to deploy an executable 
     jar named taxes.jar whose main program displays the total number of income tax 
@@ -16,8 +23,24 @@ public class Main {
         }
         
         final String pattern = "([\\w]+\\.\\d+)(?:[^,]*,){119}([\\w]+\\.\\d+),([\\w]+\\.\\d+)";
-               
-       
+        final Pattern patt = Pattern.compile(pattern);
+        String N1, N11901, A11901;
+        
+        try (BufferedReader br = new BufferedReader(new FileReader(args[0]))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                Matcher m = patt.matcher(line);
+                if (m.find()) {
+                    N1 = m.group(1);
+                    N11901 = m.group(2);
+                    A11901 = m.group(3);
+                }
+            }
+        } catch (FileNotFoundException fnfe){
+            System.out.println("File not found");
+        } catch (IOException ioe) {
+            System.out.println("Error");
+        }
     }
     
 }
