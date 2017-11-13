86f9be2b1a70cf02f1575ff52b2ef8bb04a7919b
Vinh512 <vvvhuynh@yahoo.com>
2017-11-13 07:33:21 -0600

    added search for state or zip code

--- ACC_Java_II_Day4/Main.java
+++ ACC_Java_II_Day4/Main.java
@@ -23,41 +23,67 @@ import java.util.regex.Pattern;
 public class Main {
 
     public static void main(String[] args) {
-        // ensures user provides a file argument when executing
-        if (args.length != 1) {
+        // ensures user provides a file argument and State/Zip when executing
+        if (args.length != 2) {
             System.out.println("Usage: java -jar edu.acc.java2.taxes.jar "
-                    + "[file csv]");
+                    + "[file csv] [State abbreviation or Zip Code]");
             return;
         }
         
-        // the regex matches the first group(N1), then skips the next 119 items
-        // and then matches the next couple groupings (N11901, A11901)
-        final String PATTERN = "([\\w]+\\.\\d+)(?:[^,]*,){119}([\\w]+\\.\\d+),"
-                               + "([\\w]+\\.\\d+)";
-        final Pattern PATT = Pattern.compile(PATTERN);
-        String N1, N11901, A11901;
+        // the regex matches the first group(state abbrev), then the second
+        // group(zipcode), then the third group is N1, skips the next 119 items
+        // and then matches the next couple groupings (N11901, A11901)    
+        final String PATTERN = "([A-Z]+),([\\d]+),[\\d]+,"
+                               + "([\\w]+\\.\\d+)(?:[^,]*,){119}"
+                               + "([\\w]+\\.\\d+),([\\w]+\\.\\d+)";
         
-        // initialize hashmap with keys and zero initial value
+        final Pattern PATT = Pattern.compile(PATTERN);
+        String state, zipCode, N1, N11901, A11901;       
+        
+        // initialize hashmap for user argument (state/zip)
         Map<String, Double> data = new HashMap<>();        
         data.put("N1", 0.0);
         data.put("N11901", 0.0);
         data.put("A11901", 0.0);
         
+        // intialize hashmap for grand total of entire file
+        Map<String, Double> grandTotal = new HashMap<>();        
+        grandTotal.put("N1", 0.0);
+        grandTotal.put("N11901", 0.0);
+        grandTotal.put("A11901", 0.0);
+        
         try (BufferedReader br = new BufferedReader(new FileReader(args[0]))) {
+            System.out.println("Calculating totals...\n");
             String line;
-            System.out.println("Calculating totals...");
+            
             while ((line = br.readLine()) != null) {
                 Matcher m = PATT.matcher(line);       
                 
                 if (m.find()) {
-                    N1     = m.group(1);
-                    N11901 = m.group(2);
-                    A11901 = m.group(3);
-                    
-                    calcTotal(data, N1, N11901, A11901);   
+                    state   = m.group(1);
+                    zipCode = m.group(2);
+                    N1      = m.group(3);
+                    N11901  = m.group(4);
+                    A11901  = m.group(5);
+
+                    // gets total for the state or zip
+                    if (state.equals(args[1]) || zipCode.equals(args[1])) {
+                        calcTotal(data, N1, N11901, A11901);
+                    }     
+                    // gets the grand total for the file
+                    calcTotal(grandTotal, N1, N11901, A11901);
                 }
             }            
-            displayTotals(data);
+            
+            // end program if no records were found
+            if (data.get("N1") == 0.0) {
+                System.out.println("Nothing found for " + args[1] + "!");
+                return;
+            }
+            
+            displayTotals(data, args[1]); // shows state/zip total
+            System.out.println("----");
+            displayTotals(grandTotal, "gTotal"); // shows grand total
             
         } catch (FileNotFoundException fnfe){
             System.out.println("File not found");
@@ -67,23 +93,32 @@ public class Main {
     }
     
     // updates hashmap with the accumulated totals
-    private static void calcTotal(Map<String, Double> data, String N1,
-                               String N11901, String A11901) {          
-        data.put("N1", data.get("N1") + Double.parseDouble(N1));
-        data.put("N11901", data.get("N11901") + Double.parseDouble(N11901));
-        data.put("A11901", data.get("A11901") + Double.parseDouble(A11901));
+    private static void calcTotal(Map<String, Double> mapData, String N1,
+                               String N11901, String A11901) {                
+        
+        mapData.put("N1", mapData.get("N1") + Double.parseDouble(N1));
+        mapData.put("N11901", mapData.get("N11901") + Double.parseDouble(N11901));
+        mapData.put("A11901", mapData.get("A11901") + Double.parseDouble(A11901));
     }
     
-    private static void displayTotals(Map<String, Double> data) {
-        // formats the totals like a currency with commas
-        DecimalFormat df = new DecimalFormat("###,###,###,###,###.00");
+    // displays the total based on the hashmap passed in
+    private static void displayTotals(Map<String, Double> mapData, String args) {
+        // formats currency with commas
+        DecimalFormat currency = new DecimalFormat("$###,###,###,###,###.00");
+        // formats number quantity with commas
+        DecimalFormat quantity = new DecimalFormat("###,###,###,###");
 
-        String total_N1     = df.format(data.get("N1"));
-        String total_N11901 = df.format(data.get("N11901"));
-        String total_A11901 = df.format(data.get("A11901"));
+        String total_N1     = quantity.format(mapData.get("N1"));
+        String total_N11901 = quantity.format(mapData.get("N11901"));
+        String total_A11901 = currency.format(mapData.get("A11901"));
 
-        System.out.printf("The total of N1 is: $%s\n", total_N1);
-        System.out.printf("The total of N11901 is: $%s\n", total_N11901);
-        System.out.printf("The total of A11901 is: $%s\n", total_A11901);
+        if (args.equals("gTotal")) {
+            System.out.println("The grand totals for the 2014 tax year: ");
+        } else {
+            System.out.printf("For %s, the totals are: \n", args);
+        }
+        System.out.printf("N1    : %s\n", total_N1);
+        System.out.printf("N11901: %s\n", total_N11901);
+        System.out.printf("A11901: %s\n", total_A11901);
     }   
 }
